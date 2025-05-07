8c98db42a501ede223761455bcf3d30947e1575f
Romanov <vishalmishra7828@gmail.com>
2025-05-07 20:40:40 +0530

    Initial commit 1

--- Question 1/Backend/index.js
+++ Question 1/Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use('/api', apiRoutes);
 
