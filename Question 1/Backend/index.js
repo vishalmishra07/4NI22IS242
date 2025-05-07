8ade0987968a60e29437b6936f00e2f37c30938f
Romanov <vishalmishra7828@gmail.com>
2025-05-07 20:42:06 +0530

    Initial commit 1

--- Question 1/Backend/index.js
+++ Question 1/Backend/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+
 const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use('/api', apiRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+// Use API routes
+app.use('/', apiRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
